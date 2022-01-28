4da08fbe178f6ba25586a07a99a205517c121561
Gaël <gael77@hotmail.fr>
2022-01-28 17:21:24 +0100

    Ajout données, CSS

--- script.js
+++ script.js
@@ -4,13 +4,12 @@ const contenu = document.querySelector(".contenu")
 const containerButton = document.getElementById("containerButton")
 const contentButton = document.getElementById("contentButton")
 
-let array1 = ["Potion", "Elixir", "Essence", "Philtre","Poison", "Nectar", "Flacon", "Concentré", "Jus", "Extrait", "Eau", "Huile", "Distillation", "Sérum", "Alcool", "Breuvage", "Quintessence", "Liqueur", "Bouteille", "Fiole", "Graines", "Poudre", "Secret", "Mystère", "Enchantement", "Ensorcellement", "Prophétie", "Décoction", "Infusion"]
+let array1 = ["Potion", "Elixir", "Essence", "Philtre","Poison", "Nectar", "Flacon", "Concentré", "Jus", "Extrait", "Eau", "Huile", "Distillation", "Sérum", "Alcool", "Breuvage", "Quintessence", "Liqueur", "Bouteille", "Fiole", "Graines", "Poudre", "Secret", "Mystère", "Enchantement", "Ensorcellement", "Prophétie", "Décoction", "Infusion", "Poussière", "Perles"]
 
 let array2 = ["d'Amour", "de Vitalité", "de Chance", "de Bonheur", "d'Arc en ciel", "de Lumière", "de Puissance", "de Fertilité", "de Passion", "de Nuit", "de Vérité", "de Tendresse", "de Coup de foudre", "de Joie", "de Rire", "de Rêve", "de Vigueur", "de Complicité", "de Licorne", "de Nymphe", 
-"de Fées", "de Perlimpinpin", "de Dragon", "de Griffon"]
+"de Fées", "de Perlimpinpin", "de Dragon", "de Griffon", "d'Etoile"]
 
-console.log("Liste 1 : ", array1.length, " éléments");
-console.log("Liste 2 : ", array2.length, " éléments");
+console.log(`${array1.length} contenants / ${array2.length} contenus = ${array1.length*array2.length} combinaisons`);
 
 containerButton.addEventListener("click", generateContainer)
 contentButton.addEventListener("click", generateContent)
@@ -25,7 +24,8 @@ function generateContainer() {
 
 function generateContent() {
     let random2 = Math.random()*array2.length
-contenu.innerHTML = `<p>${array2[Math.floor(random2)]}</p>`
+    contenu.innerHTML = `<p>${array2[Math.floor(random2)]}</p>`
+    // contenu.innerHTML = `<p>d'Arc en ciel</p>`
 }
 
 generateContainer()
