20fd7c2e407c7e187ae92164d33826b08f3879f3
Gaël <gael77@hotmail.fr>
2022-08-08 14:28:46 +0200

    Add items

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const contenu = document.querySelector(".contenu")
 const containerButton = document.getElementById("containerButton")
 const contentButton = document.getElementById("contentButton")
 
-let array1 = ["Potion", "Elixir", "Essence", "Philtre","Poison", "Nectar", "Flacon", "Concentré", "Jus", "Extrait", "Eau", "Huile", "Distillation", "Sérum", "Alcool", "Breuvage", "Quintessence", "Liqueur", "Bouteille", "Fiole", "Graines", "Poudre", "Secret", "Mystère", "Enchantement", "Ensorcellement", "Prophétie", "Décoction", "Infusion", "Poussière", "Perles"]
+let array1 = ["Potion", "Elixir", "Essence", "Philtre","Poison", "Nectar", "Flacon", "Concentré", "Jus", "Extrait", "Eau", "Huile", "Distillation", "Sérum", "Alcool", "Breuvage", "Quintessence", "Liqueur", "Bouteille", "Fiole", "Graines", "Poudre", "Secret", "Mystère", "Enchantement", "Ensorcellement", "Prophétie", "Décoction", "Infusion", "Poussière", "Perles", "Magie", "Force"]
 
 let array2 = ["d'Amour", "de Vitalité", "de Chance", "de Bonheur", "d'Arc en ciel", "de Lumière", "de Puissance", "de Fertilité", "de Passion", "de Nuit", "de Vérité", "de Tendresse", "de Coup de foudre", "de Joie", "de Rire", "de Rêve", "de Vigueur", "de Complicité", "de Licorne", "de Nymphe", 
-"de Fées", "de Perlimpinpin", "de Dragon", "de Griffon", "d'Etoile"]
+"de Fées", "de Dragon", "de Griffon", "de Jackalope", "d'Elfe", "d'Hypogriffe", "de Centaure", "de Druide", "de Farfadet", "de Lutin", "de Loup-garou", "de Sirène", "d'Etoile", "de Perlimpinpin", "de Printemps", "d'Éte", "d'Automne", "d'Hiver", "de Feu", "d'Eau", "d'Air", "de Terre"]
 
 console.log(`${array1.length} contenants / ${array2.length} contenus = ${array1.length*array2.length} combinaisons`);
 
